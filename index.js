7bafa8a80f3e0b2c00e83c4de26eb1b780e2ba9c
Adella <adellakenn@gmail.com>
2019-04-17 19:36:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ function destructivelyRemoveFirstKitten(name) {
  }
 
  function removeLastKitten() {
-   return kittens.slice(3)
-   //return kittens.slice(0, kittens.length -1)
+   return kittens.slice(0, kittens.length -1)
   
  }
 
